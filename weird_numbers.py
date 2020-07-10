9822100cec6df52274c212c32e47c7d23b724fe0
ozkanyildirim <ozkanyildirim.de@gmail.com>
2020-07-10 04:17:30 +0200

    Hacker Ran Task; Python If Else

---
+++ weird_numbers.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if (n % 2) != 0 or ((n % 2) == 0 and n >= 6 and n <= 20):
+        print("Weird")
+    elif n < 1 or n > 100:
+        print("out of range")
+    else:
+        print("Not Weird")
